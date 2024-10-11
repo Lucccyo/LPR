4d7acc0ea047287df3b603ea336466be37504c8b
Loïc Pottier <loic.pottier@live.fr>
2024-10-11 19:00:01 +0200

    feat: setup clean architecture

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Ready to run !");
+import express from "express";
+import morgan from "morgan";
+import { dndRouter } from "./dnd-router";
+
+const application = express();
+
+application.use(morgan("tiny"));
+application.use("/dnd", dndRouter);
+
+application.listen(3000, () => console.log("Server is running on port 3000"));
