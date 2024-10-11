8a50f938dc0656ac6d18c56a3c2fdfac354f0403
Lucccyo <cha.git@mailo.fr>
2024-10-11 17:47:36 +0200

    add alignment class

--- src/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-export const router = Router();
-
-router.get("/", (_request: Request, response: Response) => {
-  response.json({
-    message: "OK",
-  });
-});
