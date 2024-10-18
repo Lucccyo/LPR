8974af9a2f9477df895768f4de5b000928cb0758
Thomas Los <t.los@reyouzz.fr>
2024-10-18 16:43:51 +0200

    chore: formatting

--- src/infrastructure/repositories/DndApiCharClassesRepository.ts
+++ src/infrastructure/repositories/DndApiCharClassesRepository.ts
@@ -1,7 +1,7 @@
 import { CharClassRepository } from "../../domain/repositories/CharClassRepository";
 import { CharacterClass } from "../../domain/entities/characterClass";
 
-import { Adapter } from '../Adapter';
+import { Adapter } from "../Adapter";
 const adapter = new Adapter();
 
 type DataClass = {
@@ -12,24 +12,24 @@ type DataClass = {
   saving_throws: { name: string }[];
   spellcasting_ability?: { url: string };
   spells?: string;
-}
+};
 
 type AllClassesBox = {
   results: { url: string }[];
-}
+};
 
 export class DndApiCharClassRepository implements CharClassRepository {
   private readonly apiUrl = "https://www.dnd5eapi.co/api/classes";
 
   public async fetchAll(): Promise<CharacterClass[]> {
-    let characterClasses : CharacterClass[] = [];
+    const characterClasses: CharacterClass[] = [];
     try {
       const globalResponse = await fetch(this.apiUrl);
-      const data: AllClassesBox = await globalResponse.json() as AllClassesBox;
-       for (const result of data.results) {
+      const data: AllClassesBox = (await globalResponse.json()) as AllClassesBox;
+      for (const result of data.results) {
         const class_url = result.url;
-        const class_response   = await fetch(`https://www.dnd5eapi.co${class_url}`);
-        const class_data = await class_response.json() as DataClass;
+        const class_response = await fetch(`https://www.dnd5eapi.co${class_url}`);
+        const class_data = (await class_response.json()) as DataClass;
         if (class_data.spells) {
           const spells_response = await fetch(`https://www.dnd5eapi.co${class_data.spells}`);
           await adapter.deserializeSpells(spells_response);
@@ -42,10 +42,9 @@ export class DndApiCharClassRepository implements CharClassRepository {
         characterClasses.push(charClass);
       }
     } catch (error) {
-      console.error('Error fetching character classes:', error);
+      console.error("Error fetching character classes:", error);
       throw error;
     }
     return characterClasses;
   }
 }
-
